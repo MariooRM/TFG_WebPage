fe8e07191e7e99d33c052e842352b1330dc386ce
MariooRM <mariorodrg@usal.es>
2024-04-01 16:25:35 +0200

    New project

---
+++ tfg-webpage/src/firebase-config.js
@@ -0,0 +1,25 @@
+/** FIREBASE INTEGRATION */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIVCKtuTfDwXs5UsDAET-MvhYvRqOqtAQ",
+  authDomain: "tfg-mariorodrigomarcos.firebaseapp.com",
+  projectId: "tfg-mariorodrigomarcos",
+  storageBucket: "tfg-mariorodrigomarcos.appspot.com",
+  messagingSenderId: "1009482475916",
+  appId: "1:1009482475916:web:36d1109a183ed07f5a0ea9",
+  measurementId: "G-P7BEKHTTQ8"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+export default firebaseApp;
