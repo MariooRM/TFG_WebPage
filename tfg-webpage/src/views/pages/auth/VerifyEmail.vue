d2c19d5e36de4d05411fb705ec27b9c039581573
MariooRM <mariorodrg@usal.es>
2024-07-24 17:36:57 +0200

    Verify email view updated

--- tfg-webpage/src/views/pages/auth/VerifyEmail.vue
+++ tfg-webpage/src/views/pages/auth/VerifyEmail.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-      <p v-if="loading">Verifying email...</p>
-      <p v-if="success">Your email has been verified successfully!</p>
-      <p v-if="error">{{ error }}</p>
+    <div class="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div class="flex flex-column align-items-center justify-content-center">
+            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, black 10%, rgba(33, 150, 243, 0) 30%)">
+                <div class="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style="border-radius: 53px">
+                    <div class="grid flex flex-column align-items-center">
+                        <div class="flex justify-content-center align-items-center bg-black border-circle" style="height: 3.2rem; width: 3.2rem; background-color: black;">
+                            <i class="pi pi-fw pi-verified text-2xl text-white"></i>
+                        </div>
+                        <h1 class="text-900 font-bold text-5xl mb-2">Email verified</h1>
+                        <span class="text-600 mb-5">You can now close this window</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </template>
+    <AppConfig simple />
+</template>
   
   <script setup>
+  import AppConfig from '@/layout/AppConfig.vue';
   import { ref, onMounted } from 'vue';
   import { useRoute } from 'vue-router';
   import { getAuth, applyActionCode } from 'firebase/auth';
